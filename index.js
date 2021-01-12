b6350e94a611562cdfd0057379c3072536cd0227
Emil Malm <emilamalm@gmail.com>
2021-01-12 15:18:35 +0100

    :bug: removed 'mogoose' package. :sparkles: added mongoose and dotenv package. Made .env and entered port and connection_string to db.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+// Libraries
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+// Variables
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Database connection
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => {
+    console.log('Connected to DB!');
+}).catch((err) => {
+    console.log(err);
+});
+
+// Middleware
+app.use(morgan('dev'));
+
+// Route handling
 app.get('/', (req, res) => {
     res.json({msg: 'Work in progress'});
 });
 
+// Start server
 app.listen(PORT, () => console.log('Server is listening on port: ' + PORT));
